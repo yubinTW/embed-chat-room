e45f8f53d04969d36d420c830eef5240259fb928
許郁彬 <yubin@xuyubinde-MacBook-Air.local>
2018-06-11 17:54:31 +0800

    remove ckeditor

--- dodo-chat.js
+++ dodo-chat.js
@@ -157,4 +157,6 @@ $(function() {
     // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
     // It can be called as many times as necessary; previously converted input fields will not be converted again
     window.emojiPicker.discover();
+    
+    $(".emoji-wysiwyg-editor").focus(function() { $("#cke_editor1").remove(); });
 });
