93d660d5f895d67ad0d526c469e9f62207a5a32f
許郁彬 <yubin@xuyubinde-MacBook-Air.local>
2018-06-11 17:09:43 +0800

    add emoji-picker

--- dodo-chat.js
+++ dodo-chat.js
@@ -144,3 +144,17 @@ function show_chat_room () {
     document.getElementById('chat-room').classList.toggle('hide');
     chatContent.scrollTop = chatContent.scrollHeight;
 } // end of show_chat_room
+
+// setting emoji picker
+$(function() {
+    // Initializes and creates emoji set from sprite sheet
+    window.emojiPicker = new EmojiPicker({
+      emojiable_selector: '[data-emojiable=true]',
+      assetsPath: './lib/img/',
+      popupButtonClasses: 'fa fa-smile-o'
+    });
+    // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+    // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+    // It can be called as many times as necessary; previously converted input fields will not be converted again
+    window.emojiPicker.discover();
+});
