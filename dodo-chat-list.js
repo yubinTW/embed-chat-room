ec2364b2ddbbdef354f1eb5435e32e50b4e790c4
許郁彬 <yubin@xuyubins-MacBook-Air.local>
2018-10-16 18:59:25 +0800

    try fix emoji

--- dodo-chat-list.js
+++ dodo-chat-list.js
@@ -66,16 +66,7 @@ if (showChatRoom) {
 		$("#chat-room-modal").modal({
 		  fadeDuration: 100
         });
-        
-        // Initializes and creates emoji set from sprite sheet
-        window.emojiPicker = new EmojiPicker({
-            emojiable_selector: '[emojiable=true]',
-            assetsPath: 'https://yubintw.github.io/embed-chat-room/lib/img/',
-            popupButtonClasses: 'fa fa-smile-o'
-        });
-        // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
-        // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
-        // It can be called as many times as necessary; previously converted input fields will not be converted again
+
         window.emojiPicker.discover();
 
 		articleRef = firebase.database().ref("/article/"+article_id);
@@ -200,6 +191,15 @@ function getTime() {
 
 // setting emoji picker
 $(function() {
-    
+    // Initializes and creates emoji set from sprite sheet
+    window.emojiPicker = new EmojiPicker({
+        emojiable_selector: '[emojiable=true]',
+        assetsPath: 'https://yubintw.github.io/embed-chat-room/lib/img/',
+        popupButtonClasses: 'fa fa-smile-o'
+    });
+    // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+    // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+    // It can be called as many times as necessary; previously converted input fields will not be converted again
+    window.emojiPicker.discover();
 
 });
